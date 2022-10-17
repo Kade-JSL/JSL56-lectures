9a5012ba2fba29ef48d7e0f66e3892d91d4a0986
Kade-JSL <koromegu105630@gmail.com>
2022-10-17 16:43:13 +0900

    2022-10-17

---
+++ 221011-_JAVA_AND_ETC/221017/javastudy56/javastudy/src/javastudy/Java01.java
@@ -0,0 +1,25 @@
+package javastudy;
+
+public class Java01 {
+
+	public static void main(String[] args) {
+		// 포워드슬래시 두 개로 한 줄을 주석처리하게 된다.
+		/* 참고로 여러 줄 주석처리는 이렇게 한다. */
+		/* 콘솔로 출력하고 싶다면 System.out.print(), 내지는 System.out.println()
+		Java로 출력한 결과는 콘솔 메시지로 나온다. */
+		System.out.print("안녕하세요"); // print 메소드는 콘솔 창에 괄호 안의 무언가를 출력하는 것.
+		System.out.print("*"); // System.out은 그 print가 이루어지는 개체.
+		System.out.print("**");
+		System.out.print("***"); // 문장이 끝나면 반드시 세미콜론(;)을 찍어 두자.
+		/* IDE는 수시로 컴파일을 하면서 문법적으로 틀린 부분이 있으면 빨간 줄을 쳐 준다.
+		이 빨간 줄이 컴파일 에러라는 녀석이다. */
+		/* 근데 System.out.print()는 너무나도 많이 사용된지라 자동완성도 지원한다.
+		sysout을 치고 Ctrl+Space를 치면 System.out.println()이 자동 완성된다. */
+		// 참고로 VSCode에서의 자동완성은 HTML의 emmet처럼 Tab이었다...
+		// Eclipse의 코드 실행 단축키는 Ctrl+F11임을 발견했다.
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***"); // println은 출력을 하고 나서 줄 변경(line break)을 하게 된다.
+	}
+
+}
---
+++ 221011-_JAVA_AND_ETC/221017/javastudy56/javastudy/src/javastudy/Java02.java
@@ -0,0 +1,15 @@
+// 클래스를 만들 때 main 메소드는 체크해 두도록 하자. 자동으로 메인 메소드를 코딩해서 넣어 준다.
+// 체크를 안 했다? 그냥 그럼 코딩해서 넣으면 된다.
+package javastudy;
+
+public class Java02 {
+
+    public static void main(String args[]){
+        // 그럼 이제 제어문자를 좀 알아보자.
+        // 역슬래시 \는 백스페이스 아래쪽의 화폐기호 자판이다.
+        // \t: Tab, \n: 줄 변경
+        // 저장: Ctrl+'s', 이전 단계: Ctrl+'z', 다음 단계: Ctrl+'y'
+        System.out.println("성적표");
+    }
+
+}
