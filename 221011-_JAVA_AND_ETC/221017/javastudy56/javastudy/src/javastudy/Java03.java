7f2c3dac91513f083fd8ab2aff800ff5b41f20d6
Kade-JSL <koromegu105630@gmail.com>
2022-10-17 17:17:08 +0900

    2022-10-17

---
+++ 221011-_JAVA_AND_ETC/221017/javastudy56/javastudy/src/javastudy/Java03.java
@@ -0,0 +1,13 @@
+package javastudy;
+
+public class Java03 {
+    
+	public static void main(String[] args) {
+		System.out.println("\t\t급여명세서");
+		System.out.println("----------------------------------------------");
+		System.out.println("사원번호\t이름\t직급\t급여\t상여금\t총액");
+		System.out.println("----------------------------------------------");
+		System.out.println("1101\t홍길동\t대리\t3000000\t300000\t3300000");
+	}
+
+}
