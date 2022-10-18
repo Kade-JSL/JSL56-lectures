b66b8f9c145a1b93dcb10a8c0583ecea3baf97bf
Kade-JSL <koromegu105630@gmail.com>
2022-10-18 14:47:27 +0900

    2022-10-18

---
+++ 221011-_JAVA_AND_ETC/221018/javastudy56/javastudy/src/javastudy/Java04.java
@@ -0,0 +1,16 @@
+package javastudy;
+
+public class Java04 { // 설계도
+    
+    public static void main(String args[]) {
+        /*
+        - public: 접근 제한자, 누구나 접근할 수 있는 메소드라는 뜻
+        - static: 객체 생성 없이 사용 가능한 메서드
+        - void: main() 메서드를 실행한 후 리턴해줄 값이 없음
+        - main(): 반드시 1개는 존재해야 하는 메서드
+        - String args[]: 문자열을 저장하는 배열을 의미하는 매개변수
+        */ 
+        int bno = 11; // 11이란 값을 bno란 기억장소에 저장해 두세요
+    }
+
+}
