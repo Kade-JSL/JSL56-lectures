9c5e8f1a442bf71b991b0be6c02a36b6b017bd0a
Kade-JSL <koromegu105630@gmail.com>
2022-10-20 17:01:25 +0900

    2022-10-20

--- 221011-_JAVA_AND_ETC/221020/javastudy56/javastudy/src/javastudy/Java13.java
+++ 221011-_JAVA_AND_ETC/221020/javastudy56/javastudy/src/javastudy/Java13.java
@@ -12,17 +12,36 @@ public class Java13 {
         int score = con.nextInt();
 
         if(score >= 80) {// 단순 if문: 분기가 true 아니만 false 두 개밖에 존재하지 않음 
-            System.out.println("합격입니다!");
-        } // 조건이 참이면 안에 있는 명령어를 실행하도록 제어된다.
+            System.out.println("합격입니다!"); // 조건이 참이면 안에 있는 명령어를 실행하도록 제어된다.
+        } else {
+            System.out.println("불합격입니다.");
+        }    
         */
 
+        /*
         System.out.print("정수 하나를 입력하시오: ");
         int count = con.nextInt();
 
         if(count < 0) {
             System.out.println(count + "(은)는 음수입니다.");
+        } else {
+            System.out.println(count + "(은)는 양수입니다.");
+        }
+        */
+
+        // 연습 문제
+        // 정수를 한 개 입력받아 홀수인지 짝수인지를 출력하시오.
+        System.out.print("정수 하나를 입력하시오: ");
+        int num = con.nextInt();
+
+        if(num % 2 == 0) {
+            System.out.println(num + "(은)는 짝수입니다.");
+        } else {
+            System.out.println(num + "(은)는 홀수입니다.");
         }
 
+
         con.close(); // 객체를 생성했으면 닫아 주면 좋다.
-    }
-}
+
+    } // main
+} // class
