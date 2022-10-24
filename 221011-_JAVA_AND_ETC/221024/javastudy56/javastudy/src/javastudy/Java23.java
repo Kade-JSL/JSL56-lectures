87ef1f3b67dcb519399bc1c221c5db52cdf62662
Kade-JSL <koromegu105630@gmail.com>
2022-10-24 17:37:05 +0900

    2022-10-24

---
+++ 221011-_JAVA_AND_ETC/221024/javastudy56/javastudy/src/javastudy/Java23.java
@@ -0,0 +1,44 @@
+package javastudy;
+
+import java.util.Scanner;
+
+public class Java23 {
+    
+    public static void main(String[] args) {
+        /*
+        int jt = 0, et = 0, mt = 0; // 番号ごとに点数を保存する必要がないなら、総点の変数だけ宣言してもいいではないかと。
+        Scanner sc = new Scanner(System.in);
+        for (int bno = 1; bno <= 3; bno++) {
+            System.out.print(bno + "番の国語点数は?: ");
+            jt += sc.nextInt();
+            System.out.print(bno + "番の英語点数は?: ");
+            et += sc.nextInt();
+            System.out.print(bno + "番の数学点数は?: ");
+            mt += sc.nextInt();
+        }
+        System.out.printf("国語総合: %d点, 英語総合: %d点, 数学総合: %d点", jt, et, mt);
+        sc.close();
+        */
+
+        /*
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("1から10までの合: " + sum);
+        */
+
+        
+        int sum = 0;
+        for(int i = 1; i <= 10; i++) {
+            sum += i;
+            if (i == 10) {
+                System.out.print(i + "=");
+            } else {
+                System.out.print(i + "+");
+            }
+        }
+        System.out.print(sum);
+        
+    }
+}
---
+++ 221011-_JAVA_AND_ETC/221024/javastudy56/javastudy/src/javastudy/Java24.java
@@ -0,0 +1,24 @@
+﻿package javastudy;
+
+import java.util.Scanner;
+
+public class Java24 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inc = 0, sum = 0, cnt = 0; double avg = 0.0;
+        // avgは実際出力だけで使われるので、宣言する必要まではないんですが、コードの可読性のため宣言しました。
+        while (inc != -1) { // incが-1じゃない限り、ループは抜けません。
+            System.out.print("次の整数：");
+            inc = sc.nextInt(); // 抜けるにはここに-1を入力します。
+            if (inc == -1) {
+                break; // ループ文を抜けます。
+            }
+            cnt++;
+            sum += inc;
+        }
+        avg = (double)sum / (double)cnt;
+        System.out.println("平均 = " + avg);
+        sc.close();
+    }
+}
