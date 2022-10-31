292ad62ec6c72011f2abf4e79494fb1d0b17e090
Kade-JSL <koromegu105630@gmail.com>
2022-10-31 17:38:51 +0900

    2022-10-31

--- 221011-_JAVA/221031/javastudy56/javastudy/src/javastudy/Object01.java
+++ 221011-_JAVA/221031/javastudy56/javastudy/src/javastudy/Object01.java
@@ -7,12 +7,12 @@ public class Object01 { // 設計図！
         // それでメモもなるべく日本語でするようにしました。
         // mainメソッドとは？ staticはなんなの？
         // 일본어 메모: 수업 필기에서의 주석을 직역하여 적었습니다.
-        // メソッドとは：特定な作業を修行する一連の文を一つの結んだもの
+        // メソッドとは：特定な作業を行う一連の文を一つの結んだもの
 
         // main()メソッド
         // Javaプログラムの実行の始めを知らせるメソッド
         // 必ずstatic、必ずpublic、戻り値は必ずvoid、引数は必ずString型の配列
-        // public: アクセス制御子(private, default, protected, public)
+        // public: アクセス修飾子(private, default, protected, public)
         // static: オブジェクトの生成なしで使用できるメソッド
         // void: 呼び出した所に返す値がない
         // 引数: main()を呼び出した時に受け取る値を保存する変数
