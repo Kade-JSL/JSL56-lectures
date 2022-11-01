f73286214b8e50109dfe7b0f81abc32a629ba74c
Kade-JSL <koromegu105630@gmail.com>
2022-11-01 09:59:12 +0900

    2022-11-01

--- 221011-_JAVA/221031/javastudy56/javastudy/src/javastudy/Object03.java
+++ 221011-_JAVA/221031/javastudy56/javastudy/src/javastudy/Object03.java
@@ -1,7 +1,19 @@
 ﻿package javastudy;
 
 public class Object03 {
+    // Method
+    // うどんを作るなら麺が必要。機能を作るならメソッド。
+    // メソッドの名前から付けよう
+    public static void foo() {
+    /* ()：引数を受け取る部分
+     * メソッド名の先頭は小文字、2語目からは先頭は大文字
+     * そしてクラス名の先頭は大文字
+     * コンパイルが出来ないわけではない。
+     * これは不文律。プログラマー同士のルールである。
+     */
+
+    }
     public static void main(String[] args) {
-        
+
     }
 }
