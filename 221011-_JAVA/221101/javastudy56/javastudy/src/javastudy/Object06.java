def92c6570511f964a5392d5115597a370e51bd0
Kade-JSL <koromegu105630@gmail.com>
2022-11-01 12:42:44 +0900

    2022-11-01

--- 221011-_JAVA/221101/javastudy56/javastudy/src/javastudy/Object06.java
+++ 221011-_JAVA/221101/javastudy56/javastudy/src/javastudy/Object06.java
@@ -1,24 +1,26 @@
 ﻿package javastudy;
 
 public class Object06 {
-    static int getTotal(int... var) {
-        int tot = 0;
-        for (int i = 0; i < var.length; i++) {
-            tot += var[i];
-        }
-        return tot;
+    
+    static int getTotal(int a, int b) {
+        return a + b;
     }
+
     static int getSum(String a, int b, int c) {
         return Integer.parseInt(a) + b + c;
     }
+
     static double getAve(int sum, int num) {
         return (double)sum / (double) num;
     }
+
     public static void main(String[] args) {
         int total = getTotal(10, 20);
         System.out.println("total: " + total);
+
         int sum = getSum("10", 20, 30);
         System.out.println("sum: " + sum);
+
         double average = getAve(sum, 3);
         System.out.println("average: " + average);
     }
