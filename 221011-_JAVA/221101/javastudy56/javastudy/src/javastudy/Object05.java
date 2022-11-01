e22409554fc1f4d97c52f2012a3b633e9d533b67
Kade-JSL <koromegu105630@gmail.com>
2022-11-01 11:48:59 +0900

    2022-11-01

---
+++ 221011-_JAVA/221101/javastudy56/javastudy/src/javastudy/Object05.java
@@ -0,0 +1,16 @@
+﻿package javastudy;
+
+public class Object05 {
+    /*
+    public static int getSum(int... par) {
+        int sum = 0;
+        for (int i = 0; i < par.length ; i++) {
+            sum += par[i];
+        }
+        return sum;
+    }
+    */
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt("50점"));
+    }
+}
