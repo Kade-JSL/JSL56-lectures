5d71a011a3ad7dc279db7cc6f3d23211cdbb2c38
Kade-JSL <koromegu105630@gmail.com>
2022-11-02 12:47:55 +0900

    2022-11-02

--- 221011-_JAVA/221102/javastudy56/javastudy/src/javastudy/Object07.java
+++ 221011-_JAVA/221102/javastudy56/javastudy/src/javastudy/Object07.java
@@ -1,12 +1,25 @@
 ﻿package javastudy;
 
 public class Object07 {
-    static int getTotal(int a, String b, int c){
+    static int getTotal(int a, String b, int c) {
         return a + Integer.parseInt(b) + c;
     }
+    static double getAve(int a, String b) {
+        return (double)a / (double)Integer.parseInt(b);
+    }
+    static String getResult(double avg) {
+        if (avg > 90) return "수";
+        else if (avg > 80) return "우";
+        else if (avg > 70) return "미";
+        else if (avg > 60) return "양";
+        else return "가";
+    }
     public static void main(String[] args) {
-        int x = new Integer("");
         int total = getTotal(55, "60", 85);
-        System.out.println("총점: " + total + "...." + x);
+        System.out.println("총점: " + total);
+        double average = getAve(total, "3");
+        System.out.println("평균: " + average);
+        String result = getResult(average);
+        System.out.println("결과: " + result);
     }
 }
