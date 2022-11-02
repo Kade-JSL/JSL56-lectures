cb6a59d16f2e2c82a3bcf329168e03b4738eb5cc
Kade-JSL <koromegu105630@gmail.com>
2022-11-02 12:00:20 +0900

    2022-11-02

--- 221011-_JAVA/221102/javastudy56/javastudy/src/javastudy/Object07.java
+++ 221011-_JAVA/221102/javastudy56/javastudy/src/javastudy/Object07.java
@@ -1,5 +1,11 @@
 ﻿package javastudy;
 
 public class Object07 {
-    
+    static int getTotal(int a, String b, int c){
+        return a + Integer.parseInt(b) + c;
+    }
+    public static void main(String[] args) {
+        int total = getTotal(55, "60", 85);
+        System.out.println("총점: " + total);
+    }
 }
