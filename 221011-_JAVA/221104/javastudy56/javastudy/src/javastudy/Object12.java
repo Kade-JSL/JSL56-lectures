218a37d45f83db158f840deab0d23472389c9a5e
Kade-JSL <koromegu105630@gmail.com>
2022-11-04 12:47:49 +0900

    2022-11-04

--- 221011-_JAVA/221104/javastudy56/javastudy/src/javastudy/Object12.java
+++ 221011-_JAVA/221104/javastudy56/javastudy/src/javastudy/Object12.java
@@ -6,9 +6,17 @@ public class Object12 {
          * Scanner 객체를 부르는 이유: next()랑 nextInt()를 쓰고 싶어서!
          * 역시 Object12_sub 객체를 부르는 이유도 그 안에 있는 getTotal(...)을 쓰고 싶어서이다.
          */
-        Object12_car car = new Object12_car();
-        System.out.println("color " + car.getColor());
-        car.setColor("Red");
-        System.out.println("color " + car.getColor());
+        Object12_car myCar = new Object12_car();
+        System.out.println("myCar color: " + myCar.getColor());
+        // myCar.setColor("yellow");
+        // myCar.setColor("Red");
+
+        Object12_car yourCar = new Object12_car("yellow");
+        System.out.println("yourCar color: " + yourCar.getColor());
+        System.out.println("yourCar speed: " + yourCar.getSpeed());
+
+        Object12_car ourCar = new Object12_car("white", 150);
+        System.out.println("ourCar color: " + ourCar.getColor());
+        System.out.println("ourCar speed: " + ourCar.getSpeed());
     }    
 }
