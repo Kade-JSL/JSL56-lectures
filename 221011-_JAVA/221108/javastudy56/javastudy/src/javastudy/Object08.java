90a37d896d9294d36d7c2553454ad441efb7ef8e
Kade-JSL <koromegu105630@gmail.com>
2022-11-08 15:18:49 +0900

    2022-11-08

--- 221011-_JAVA/221108/javastudy56/javastudy/src/javastudy/Object08.java
+++ 221011-_JAVA/221108/javastudy56/javastudy/src/javastudy/Object08.java
@@ -2,7 +2,7 @@
 
 public class Object08 {
     public static void main(String[] args) {
-        Object07 hong = new Object07();
-        System.out.println(hong.getName());
+        Object07 obj7 = new Object07(); // 클래스 객체 생성, 반드시 new라는 예약어를 사용
+        System.out.println(obj7.getName());
     }
 }
