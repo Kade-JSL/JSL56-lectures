4dd890a90aee93b3dee58c43f46d9e52ebab3c4f
Kade-JSL <koromegu105630@gmail.com>
2022-11-14 16:09:46 +0900

    2022-11-14

--- 221011-_JAVA/221114/javastudy/src/controller/Main.java
+++ 221011-_JAVA/221114/javastudy/src/controller/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student stu = new Student(); // private 멤버들은 클래스 밖에선 접근할 수 없음
+        // private 멤버들은 클래스 밖에선 접근할 수 없음
         // 참고로 아무 것도 안 붙이면 패키지 밖에선 접근할 수 없다. import를 한대도 말이다.
+
+        Student stu = new Student(1111, "스탑 포스팅", "어바웃 어몽 어스", "아임 타이얼드 옵 씨잉 잇");
+        stu.setNum(2222);
+        System.out.println(stu.getNum());
     }
 }
--- 221011-_JAVA/221114/javastudy/src/dto/Student.java
+++ 221011-_JAVA/221114/javastudy/src/dto/Student.java
@@ -5,6 +5,15 @@ public class Student {
     private int num;
     private String name, address, tel;
 
+    public Student() {}
+
+    public Student(int num, String name, String address, String tel) {
+        this.num = num;
+        this.name = name;
+        this.address = address;
+        this.tel = tel;
+    }
+
     // getters and setters
     public int getNum() {
         return num;
@@ -12,21 +21,27 @@ public class Student {
     public void setNum(int num) {
         this.num = num;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getTel() {
         return tel;
     }
+
     public void setTel(String tel) {
         this.tel = tel;
     }
