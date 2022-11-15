f8b4f0460cc40b1b193eb0aa2bfe5e5043dc05de
Kade-JSL <koromegu105630@gmail.com>
2022-11-15 19:25:16 +0900

    2022-11-15

--- 221011-_JAVA/221115/javastudy/controller/SingletonMain.java
+++ 221011-_JAVA/221115/javastudy/controller/SingletonMain.java
@@ -12,7 +12,7 @@ public class SingletonMain {
         Connection c = DBConn.getInstance().getConnection(); // 열거형. chain 방식이라 부르기도 함.
         // 객체 생성 없음. 포인터로만 가리킴.
         // 포인터로 가서 가리키는 객체의 메서드를 실행하여 리턴값을 받음
-        // 던져진 예외를 받음: 또 던진다
+        // getConnection()에서 던져진 예외를 받음: 또 던진다
         if (c != null) {
             System.out.println("오라클 DB에 접속되어 DB에 대한 CRUD 작업 처리가 가능해졌다!");
         }
