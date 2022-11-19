8fa9194d9e211ce2889ac5736962ef70c2b6a909
Kade-JSL <koromegu105630@gmail.com>
2022-11-19 15:56:44 +0900

    temporary

--- 221011-_JAVA/221118/javastudy/controller/InterfaceMain.java
+++ 221011-_JAVA/221118/javastudy/controller/InterfaceMain.java
@@ -4,87 +4,85 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import dto.Student;
 
-interface 점수 { // 인터페이스
+interface ScoreInterfaces { // 인터페이스
 
     int 갯수 = 10; // 상수
 
-    /* public abstract */ void 성적입력(); // 따로 접근 제한자를 두지 않아도 인터페이스에선 모두 이런 취급이다
+    /* public abstract */ void scoreInput(); // 따로 접근 제한자를 두지 않아도 인터페이스에선 모두 이런 취급이다
 
-    void 성적계산();
+    void scoreCalc();
 
-    void 성적출력();
+    void printScore();
 }
 
 
-class 점수처리 extends Student implements 점수 {
+class ScoreCalc extends Student implements ScoreInterfaces {
 
-    ArrayList<Student> 학생목록 = new ArrayList<Student>();
-    Scanner 스캐너 = new Scanner(System.in);
+    ArrayList<Student> studentList = new ArrayList<Student>();
+    Scanner scanner = new Scanner(System.in);
 
-    int 번호 = 1110;
+    int bno = 1110;
 
     @Override
-    public void 성적입력() {
-        System.out.println("학번: " + ++번호);
+    public void scoreInput() {
+        System.out.println("학번: " + ++bno);
         System.out.print("이름: ");
-        String name = 스캐너.next();
+        String name = scanner.next();
         System.out.print("주소: ");
-        String address = 스캐너.next();
+        String address = scanner.next();
         System.out.print("전화번호: ");
-        String tel = 스캐너.next();
+        String tel = scanner.next();
         System.out.print("국어: ");
-        int kor = 스캐너.nextInt();
+        int kor = scanner.nextInt();
         System.out.print("영어: ");
-        int eng = 스캐너.nextInt();
+        int eng = scanner.nextInt();
         System.out.print("수학: ");
-        int mat = 스캐너.nextInt();
+        int mat = scanner.nextInt();
 
-
-
-        Student 학생 = new Student();
-        학생.setNum(번호);
-        학생.setName(name);
-        학생.setAddress(address);
-        학생.setTel(tel);
-        학생.setKor(kor);
-        학생.setEng(eng);
-        학생.setMat(mat);
-        학생목록.add(학생);
+        Student student = new Student();
+        student.setNum(bno);
+        student.setName(name);
+        student.setAddress(address);
+        student.setTel(tel);
+        student.setKor(kor);
+        student.setEng(eng);
+        student.setMat(mat);
+        studentList.add(student);
     }
 
     @Override
-    public void 성적계산() {
+    public void scoreCalc() {
         // 총점과 평균 계산
-        for (Student 학생 : 학생목록) {
+        for (Student s : studentList) {
             int tot = 0;
-            for (Student.Subjects 과목 : Student.Subjects.values()) {
-                switch (과목) {
+            for (Student.Subjects subj : Student.Subjects.values()) {
+                switch (subj) {
                     case KOR:
-                        tot += 학생.getKor();
+                        tot += s.getKor();
                         break;
                     case ENG:
-                        tot += 학생.getEng();
+                        tot += s.getEng();
                         break;
                     case MAT:
-                        tot += 학생.getMat();
+                        tot += s.getMat();
                         break;
                 }
             }
-            double avg = (double) tot / (double) Student.Subjects.values().length;
-            학생.setTot(tot);
-            학생.setAvg(avg);
+            double avg = (double)tot / (double)Student.Subjects.values().length;
+            s.setTot(tot);
+            s.setAvg(avg);
         }
     }
 
     @Override
-    public void 성적출력() {
+    public void printScore() {
         System.out.println("성적표");
         System.out.println("번호\t이름\t주소\t전화\t국어\t영어\t수학\t총점\t평균");
-        
-        for (Student 학생 : 학생목록) {
-            System.out.println(학생.getNum() + "\t " + 학생.getName() + "\t " + 학생.getAddress() + "\t "
-                    + 학생.getTel() + "\t " + 학생.getKor() + "\t " + 학생.getEng() + "\t " + 학생.getMat()
-                    + "\t " + 학생.getTot() + "\t " + 학생.getAvg());
+
+        for (Student s : studentList) {
+            System.out.println(s.getNum() + "\t " + s.getName() + "\t " + s.getAddress() + "\t "
+                    + s.getTel() + "\t " + s.getKor() + "\t " + s.getEng() + "\t " + s.getMat()
+                    + "\t " + s.getTot() + "\t " + s.getAvg());
         }
     }
 
@@ -93,8 +91,8 @@ class 점수처리 extends Student implements 점수 {
 
 public class InterfaceMain {
     public static void main(String[] args) {
-        점수처리 학생목록 = new 점수처리();
-        Scanner 스캐너 = new Scanner(System.in);
+        ScoreCalc scoreCalc = new ScoreCalc();
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("메뉴");
@@ -102,25 +100,25 @@ public class InterfaceMain {
             System.out.println("2. 성적출력");
             System.out.println("0(or etc.). 종료");
             System.out.print("작업 번호 선택 >> ");
-            int 메뉴 = 스캐너.nextInt();
-            if (메뉴 == 1) {
+            int menu = sc.nextInt();
+            if (menu == 1) {
                 while (true) {
-                    학생목록.성적입력();
-                    학생목록.성적계산();
+                    scoreCalc.scoreInput();
+                    scoreCalc.scoreCalc();
                     System.out.print("더 입력하려면 1을 입력하세요: ");
-                    int 추가입력여부 = 스캐너.nextInt();
-                    if (추가입력여부 != 1) {
+                    int addInput = sc.nextInt();
+                    if (addInput != 1) {
                         break;
                     }
                 }
-            } else if (메뉴 == 2) {
-                학생목록.성적출력();
+            } else if (menu == 2) {
+                scoreCalc.printScore();
             } else {
                 break;
             }
         }
 
         System.out.println("프로그램을 종료합니다.");
-        스캐너.close();
+        sc.close();
     }
 }
