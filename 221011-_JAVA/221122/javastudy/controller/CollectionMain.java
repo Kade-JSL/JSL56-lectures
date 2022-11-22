dc5ae115461b19995d6427579cb8ac7fdf3ad586
Kade-JSL <koromegu105630@gmail.com>
2022-11-22 16:45:26 +0900

    2022-11-22

--- 221011-_JAVA/221122/javastudy/controller/CollectionMain.java
+++ 221011-_JAVA/221122/javastudy/controller/CollectionMain.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class CollectionMain {
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<>(); 
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(); 
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        System.out.println(arrayList.size());
+        int sum = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.print(arrayList.get(i) + " ");
+            sum += arrayList.get(i);
+        }
+        System.out.println("\nsum = " + sum);
     }
 }
