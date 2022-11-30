e8a0060d8977c53c99c93d9b68f691e24cd9bd65
Kade-JSL <koromegu105630@gmail.com>
2022-11-30 17:36:23 +0900

    2022-11-30

--- 221125-_JAVA_INTERMEDIATE/221130/javastudy/controller/StudentMain.java
+++ 221125-_JAVA_INTERMEDIATE/221130/javastudy/controller/StudentMain.java
@@ -7,29 +7,25 @@ import dto.Student;
 
 public class StudentMain {
 
-    static void prompt() {
-        System.out.println("해당 학생을 찾을 수 없습니다. 다시 시도해 주세요.");
-    }
-
-    static void prompt(char crud, int pindex) {
-        String crudString = "";
-        switch (crud) {
-            case 'r': System.out.println("학번\t이름\t주소\t\t번호\t국어\t영어\t수학\t총점\t평균\t결과"); return;
-            case 'u': crudString = "수정"; break;
-            case 'd': crudString = "삭제"; break;
-            default: return;
+    static void prompt(String s) {
+        String crudString = s.substring(0, 2);
+        if (crudString.equals("학생")) {
+            if (s.substring(2).equals("못찾음")) {
+                System.out.println("해당 학생을 찾을 수 없습니다. 다시 시도해 주세요.");
+                return;
+            } else if (s.substring(2).equals("첫줄")) {
+                System.out.println("학번\t이름\t주소\t\t번호\t국어\t영어\t수학\t총점\t평균\t결과");
+                return;
+            }
         }
-        switch (pindex) {
-            case 0:
-                System.out.print(crudString + "하고자 하는 학번을 입력하세요 >> ");
-                break;
-            case 1:
-                System.out.print("정상적으로 " + crudString + "(이)가 완료되었습니다.\n");
-                break;
-            case 2:
-                System.out.print("정상적으로 " + crudString + "(이)가 완료되지 않았습니다. 다시 시도해 주세요.\n");
-                break;
-            default: return;
+        if (s.substring(2).equals("입력")) {
+            System.out.print(crudString + "하고자 하는 학번을 입력하세요 >> ");
+        } else if (s.substring(2).equals("완료")) {
+            System.out.print("정상적으로 " + crudString + "(이)가 완료되었습니다.\n");
+        } else if (s.substring(2).equals("안완료")) {
+            System.out.print("정상적으로 " + crudString + "(이)가 완료되지 않았습니다. 다시 시도해 주세요.\n");
+        } else if (s.substring(2).equals("정말")) {
+            System.out.print("정말로 " + crudString + "하시겠습니까?[Y/n(default)] >> ");
         }
     }
 
@@ -43,6 +39,7 @@ public class StudentMain {
 
         Student stu = null;
         int inputNum = 0;
+        String ynselect = null;
 
         while (run) {
             System.out.print("[1] 입력 / [2] 출력 / [3] 수정 / [4] 삭제 / [0] 종료\n메뉴 선택 >> ");
@@ -81,24 +78,25 @@ public class StudentMain {
                     List<Student> stuList = stuDao.selectAll();
                     
                     System.out.println("학생 목록");
-                    prompt('r', 0);
+                    prompt("학생첫줄");
                     for (Student s : stuList) {
                         stuDao.read(s);
                     }
                     break;
                 case 3:
-                    prompt('u', 0);
+                    prompt("수정입력");
                     inputNum = sc.nextInt();
                     stu = stuDao.selectNum(inputNum);
                     if (stu == null) {
-                        prompt();
+                        prompt("학생못찾음");
                         break;
                     }
                     System.out.println("수정 대상 학생");
-                    prompt('r', 0);
+                    prompt("학생첫줄");
                     stuDao.read(stu);
-                    System.out.print("정말로 수정하시겠습니까(Y/n(default) >> ");
-                    if (sc.next() != "Y") break;
+                    prompt("수정정말");
+                    ynselect = sc.next();
+                    if (!ynselect.toLowerCase().equals("y")) break;
                     System.out.print("수정할 국어점수: ");
                     int ukor = sc.nextInt();
                     System.out.print("수정할 영어점수: ");
@@ -117,30 +115,26 @@ public class StudentMain {
                     else stu.setResult("불합격");
                     int updateResult = stuDao.update(stu);
                     if (updateResult > 0) {
-                        prompt('u', 1);
-                        System.out.println(" 수정 결과: ");
+                        prompt("수정완료");
+                        System.out.println("수정 결과: ");
                         stuDao.read(stu);
                     } else {
-                        prompt('u', 2);
+                        prompt("수정안완료");
                     }
                     break;
                 case 4:
-                    prompt('d', 0);
+                    prompt("삭제입력");
                     inputNum = sc.nextInt();
                     stu = stuDao.selectNum(inputNum);
-                    if (stu == null) {
-                        prompt();
-                        break;
-                    }
+                    if (stu == null) { prompt("학생못찾음"); break; }
+                    prompt("학생첫줄");
                     stuDao.read(stu);
-                    System.out.print("정말로 이 학생을 삭제하시겠습니까(Y/n(default))? >> ");
-                    if (sc.next().equals("Y")) {
-                        int result = stuDao.delete(inputNum);
-                        if (result > 0) prompt('d', 1);
-                        else prompt('d', 2); break;
-                    } else {
-                        System.out.println("삭제가 취소되었습니다.");
-                    }
+                    prompt("삭제정말");
+                    ynselect = sc.next();
+                    if (!ynselect.toLowerCase().equals("y")) { System.out.println("삭제가 취소되었습니다."); break; }
+                    int result = stuDao.delete(inputNum);
+                    if (result > 0) prompt("삭제완료");
+                    else prompt("삭제안완료");
                     break;
                 case 0:
                     System.out.println("프로그램을 종료합니다.");
