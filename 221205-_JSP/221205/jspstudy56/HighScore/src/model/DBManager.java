7c9a35b460d4e14dc99ad3a475a8f947f34d06ff
Kade-JSL <koromegu105630@gmail.com>
2022-12-05 15:59:29 +0900

    2022-12-05

--- 221205-_JSP/221205/jspstudy56/HighScore/src/model/DBManager.java
+++ 221205-_JSP/221205/jspstudy56/HighScore/src/model/DBManager.java
@@ -2,27 +2,49 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DBManager {
-    
-    private static DBManager instance = new DBManager();
-    private DBManager() {}
-    public static DBManager getInstance() { return instance; }
-
-    public Connection getConnection() {
-        Connection conn = null;
-
-        String driver = "oracle.jdbc.driver.OracleDriver";
-        String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        String id = "kade01";
-        String pw = "1234";
-
-        try {
-            Class.forName(driver);
-            conn = DriverManager.getConnection(url, id, pw);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return conn;
-    } 
+	private static DBManager instance = new DBManager();
+	private DBManager() {}
+	public static DBManager getInstance() { return instance; }
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "kade01";
+		String pw = "1234";
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, id, pw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public void close(Connection conn, PreparedStatement pstmt) {
+		try {
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+		try {
+			if (rs != null) rs.close();
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
