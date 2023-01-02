ac2edd7ff567f219d86b4fd5f5023884d5ccbe47
Kade-JSL <114974591+Kade-JSL@users.noreply.github.com>
2023-01-02 11:21:01 +0900

    2023-01-02

--- 999999_ETC/3_project/jslhrdServlet/src/com/jslhrd/utility/FormDto.java
+++ 999999_ETC/3_project/jslhrdServlet/src/com/jslhrd/utility/FormDto.java
@@ -1,13 +1,22 @@
 package com.jslhrd.utility;
 
+import java.util.HashMap;
+
 public class FormDto {
 	
 	public enum FormName {
 		TITLE,
-		
+		WRITER,
+		CONTENT
 	}
+
+	HashMap<FormName, Boolean> fnList = new HashMap<FormName, Boolean>() {
+		FormName.TITLE : false,
+		FormName.WRITER : false,
+		FormName.CONTENT : false
+	};
 	
-	public FormDto() {
-		
+	public FormDto(FormName... fnList) {
+
 	}
 }
